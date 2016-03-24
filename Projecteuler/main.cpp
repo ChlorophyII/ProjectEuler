77777caef0c2782f1355bfddbbe06f7f56453266
ChlorophyII <shiyue9621@gmail.com>
2016-03-24 20:16:01 +0800

    Initial Commit

---
+++ Projecteuler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projecteuler
+//
+//  Created by Shi Yue on 24/3/2016.
+//  Copyright © 2016 Shi Yue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
